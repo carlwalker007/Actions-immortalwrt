f4211ffcfc53433f84dd8f2f1d7b238c27633b18
Rubber <caesar.pcnto.com>
2024-04-15 17:08:33 +0800

    update

--- scripts/O23-SNAPSHOT/diy.sh
+++ scripts/O23-SNAPSHOT/diy.sh
@@ -1,38 +1,17 @@
 #!/bin/bash
 
-#删除feeds自带mosdns、v2ray-geodata
-rm -rf ./feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
+#删除feeds自带
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
 
 #删除自带u-boot.dts
 rm -rf package/boot/uboot-rockchip/src/arch/arm/dts/rk3328-orangepi-r1-plus-lts-u-boot.dtsi
 
 #更改默认地址为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-
-#更改r8152默认驱动
-#sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
-
-#添加内核配置
-sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-5.15
-
-#克隆passwall环境插件
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
-
-#克隆的源码放在small文件夹
-mkdir package/small
-pushd package/small
-
-#克隆源码
-#adguardhome
-git clone -b main --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
-#passwall2
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
-#mosdns
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
-#v2ray-geodata
-git clone https://github.com/sbwml/v2ray-geodata
-#lucky
-git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
-
-popd
+sed -i 's/192.168.1.1/192.168.2.6/g' package/base-files/files/bin/config_generate
